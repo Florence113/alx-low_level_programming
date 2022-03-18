b4c880688c9f89a2a976e5b3f2e7ff8244998aeb
Florence113 <florencejacintaa02@gmail.com>
2022-03-18 08:13:21 -0700

    Add 3-islower.c file and code

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,6 +6,12 @@
  * otherwise return 0
  */
 
+/**
+ * _is_lower - main code
+ * @c: the character is ASCII code
+ * Return: 1 for lowercase character. 0 for the rest.
+ */
+
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
