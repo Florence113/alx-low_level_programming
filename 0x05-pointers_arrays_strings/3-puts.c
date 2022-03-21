dd45436c484f8c17161a89c4fd3e19dbf62e1c28
Florence113 <florencejacintaa02@gmail.com>
2022-03-21 12:51:45 -0700

    Add 3-puts.c file and code

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_puts - prints a string, followed by a new line
+*@str: A pointer to an int that will be changed
+*
+*Return: no return
+*/
+
+void _puts(char *str)
+{
+	char *c;
+	int m;
+
+	c = str;
+
+	for (m = 0; c[m]; m++)
+	{
+		_putchar (c[m]);
+	}
+	_putchar('\n');
+}
